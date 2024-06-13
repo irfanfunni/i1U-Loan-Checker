cf1ea34c4cb813a4a4ef6eea8950ee1baf5e80ae
irfan <irfanfunni@gmail.com>
2024-06-13 19:23:48 +0800

    moved report generator function

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import time
 import random
-import report_generator
+from datetime import datetime
 
 #Configure Page
 st.set_page_config(
@@ -17,6 +17,49 @@ def border():
         st.write("")
         st.divider()
         st.write("")
+        
+#Generate report
+def generate_report (user_details,output):
+    report_date_time = f"{datetime.now():%d-%b-%Y %H:%M}"
+    #Check company age
+    if user_details["NewExist"] == 2: 
+        company_age = "Less than or equal to 2 years old"
+    elif user_details["NewExist"] == 1: 
+        company_age = "Greater than 2 years old"
+    #This is the overall report that will be generated. 
+    report = f"""\
+____________________________________________________________________________________________________
+*********************************i1Loan Loan Eligibility Checker************************************
+
+Group : i1
+____________________________________________________________________________________________________
+***************************************Report Details***********************************************
+
+Date: {report_date_time.split(" ")[0]}
+Time: {report_date_time.split(" ")[1]}
+
+____________________________________________________________________________________________________
+****************************************User Details************************************************
+
+NAICS: {user_details["NAICS"]}
+ZipCode: {user_details["Zip"]}
+Number of Employees: {user_details["NoEmp"]}
+Loan Term (in months): {user_details["Term"]}
+Company age: {company_age}
+
+___________________________________________________________________________________________________
+********************************************Outcome*************************************************
+
+{output}
+
+____________________________________________________________________________________________________
+
+Thank you for using i1's University Eligibility Checker! See you again!
+
+
+*****************************************End of Report**********************************************
+"""
+    return report
 
 #***************************************************************************************#
 #First Row
@@ -115,7 +158,7 @@ def generate_output():
             else: 
                 st.write("")
   
-        text_contents = report_generator.generate_report(st.session_state["user_data"],output)
+        text_contents = generate_report(st.session_state["user_data"],output)
         id = str(random.randint(0,100001))
         file_name = f"i1LoanEligibilityChecker_{id}.txt"
         if st.download_button("Download Report", text_contents, file_name = file_name ,disabled = not st.session_state['data_filled']):   
