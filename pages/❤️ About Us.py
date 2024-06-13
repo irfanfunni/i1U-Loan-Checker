7f30fed7a8effd1b757135970866b94a6d26cac9
irfan <irfanfunni@gmail.com>
2024-06-13 20:04:37 +0800

    changed about us

--- pages/❤️ About Us.py
+++ pages/❤️ About Us.py
@@ -17,13 +17,13 @@ st.title("About Us")
 st.subheader("Sustainability | Accessibility | Inclusivity")
 st.write("")
 st.write(f"""
-Welcome to our loan eligibility checker app! We are a team of passionate developers and data scientists who have come together to create a user-friendly solution to help individuals determine their eligibility for a loan from a bank.
+Welcome to our loan eligibility checker app! We are a team of passionate BBCS Hackathon participants who have come together to create a user-friendly solution to help individuals determine their eligibility for a loan from a bank.
 
 At our core, we believe in leveraging the power of machine learning and advanced analytics to simplify complex financial processes. Through meticulous analysis and utilizing cutting-edge libraries such as scikit-learn and PyTorch, we've developed a robust model that accurately predicts loan eligibility based on various factors.
 
 Our mission is to :orange[empower users with the knowledge they need to make informed financial decisions]. Whether you're looking to fund your dream home, pursue higher education, or expand your business, our app provides you with insights into your loan eligibility quickly and efficiently.
 
-We understand that navigating the loan application process can be daunting, which is why we've designed our :orange[app to be intuitive and user-friendly]. With just a few clicks, you can input your information and receive instant feedback on your eligibility status.
+We understand that navigating the loan application process can be daunting, which is why we've designed our :orange[app to be intuitive and user-friendly]. With just a few clicks, you can input your information and receive instant feedback on your eligibility status!
 
 Transparency and accuracy are at the forefront of everything we do. :orange[Our model is built on extensive data analysis and is continuously refined to ensure reliable results]. We are committed to providing you with a trustworthy tool that you can rely on when exploring your loan options.
 
