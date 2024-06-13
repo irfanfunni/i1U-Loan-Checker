0e641b42956e6bb6d6a49253f2416af51d24d20b
irfan <irfanfunni@gmail.com>
2024-06-13 20:14:14 +0800

    change tim

--- Home.py
+++ Home.py
@@ -95,7 +95,8 @@ get_user_details()
 output = ""
 if st.session_state['data_filled'] and st.session_state['user_data']:
     st.write("Ok can run ML liao")
-    report_date_time = f"{datetime.now():%d-%b-%Y %I:%M:%S %p}"
+    now = datetime.now()
+    report_date_time = now.strftime("%d-%b-%Y %I:%M:%S %p")
     output = f"""
 You are __________ for a bank loan with a ____% chance of success. 
 
