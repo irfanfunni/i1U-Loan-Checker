0d5f099a85b3e719c9e0eb2d0dea5e0a095620c2
irfan <irfanfunni@gmail.com>
2024-06-13 19:55:03 +0800

    change emoji

---
+++ pages/📈 Data Analysis.py
@@ -0,0 +1,21 @@
+#Import Modules
+import streamlit as st
+import pandas as pd
+
+
+#Configure Page
+st.set_page_config(
+    page_title="Data Analysis",
+    page_icon="💵",
+)
+
+st.header("Bar Chart of The Frequency Loan Requests vs the States")
+# Read the CSV file
+df = pd.read_parquet("./datasets/SBAnational_cleaned.parquet")
+
+state_counts = df["State"].value_counts()
+
+st.bar_chart(state_counts)
+st.write()
+st.divider()
+st.write()
